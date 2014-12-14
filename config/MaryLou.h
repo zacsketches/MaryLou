0e796e85ee60f3193b3bbe189c929870eb6ca72d
zacsketches <zacsketches@github.com>
2014-12-14 15:39:32 -0800

    Added the config file to the repo.
      I need to add a link in the actual Arduino/library folder so that
      this config file is updated and used.

---
+++ config/MaryLou.h
@@ -0,0 +1,40 @@
+#ifndef MARYLOU_CONFIG
+#define MARYLOU_CONFIG DEC_2014
+
+//Physical connections
+//The left motor is B on the motor shield
+#define LT_DIR_PIN  13
+#define LT_PWM_PIN  11
+#define LT_SENSE_PIN  A1
+#define LT_ENCODER_A_PIN  7
+#define LT_ENCODER_B_PIN  6
+
+//The right motor is A on the motor shield
+#define RT_DIR_PIN  12
+#define RT_PWM_PIN  3
+#define RT_SENSE_PIN  A0
+#define RT_ENCODER_A_PIN  5
+#define RT_ENCODER_B_PIN  4
+
+// 2A / 1023 ADC resolution = .00196
+// This value is used in converting values from the current sensing into
+// Amps running into the motor 
+#define CONV_I 0.00196;
+
+// Max effort taken from model of controller in MATLAB simulation
+// to disturbances...for now I'm using 255 until I run the MATLAB model
+// some more.
+#define MAX_EFFORT 255;
+
+// Deadband data taken on 12/14/14.
+// Deadband_solver with step size 10, 1500 delay, 30 starting effort and 
+// five loops.  MaryLou was suspended in the vice and connected 
+// to 12V benchtop power supply.  I probably need to run the same test on
+// the battery, and consider whether I want dead band for suspended ops
+// or with the robot weight on the wheels....but this should work for now.
+#define RT_FWD_DEAD 50
+#define RT_BCK_DEAD -72
+#define LT_FWD_DEAD 46
+#define LT_BCK_DEAD -66
+
+#endif
